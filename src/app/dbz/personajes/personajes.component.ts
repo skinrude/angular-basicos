63aa037e3f0f2f19e40bd0818d1b41bb71825e68
Victor Hugo Victoriano <ing.victor.vv@gmail.com>
2023-01-26 22:01:31 -0600

    Ajustes

---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html'
+})
+export class PersonajesComponent {
+  personajes: any[] = [];
+}
