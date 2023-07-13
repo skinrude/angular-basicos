f57d4386312425ccaeffdbd67669f8e844730890
Victor Hugo Victoriano <ing.victor.vv@gmail.com>
2023-07-12 23:54:21 -0600

    Ajustes a proyecto

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { MainPageComponent } from './main-page/main-page.component';
-import { PersonajesComponent } from './personajes/personajes.component';
+import { MainPageComponent } from './pages/main-page.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent,
-    PersonajesComponent
+    MainPageComponent
   ],
   exports: [
     MainPageComponent
--- src/app/dbz/personajes/personajes.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-personajes',
-  templateUrl: './personajes.component.html'
-})
-export class PersonajesComponent {
-  personajes: any[] = [];
-}
