f313ca46a109e7809de5434453158efe1d1769f4
Victor Hugo Victoriano <ing.victor.vv@gmail.com>
2023-07-24 18:13:00 -0600

    Ajustes para hacer private metodos

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -13,5 +13,15 @@ export class ListComponent {
     name: 'Trunks',
     power: 10
   }]
+
+  @Output()
+  onDelete: EventEmitter<string> = new EventEmitter();
+
+  public onDeleteCharacter(id?: string): void {
+
+    if (!id) return;
+    
+    this.onDelete.emit(id);
+  }
 
 }
